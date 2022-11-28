5859a4b67d1ba6d9e1d9d41e6e4e291a75b53c05
Evening <evening@mstars-lab3-05s-iMac.local>
2022-11-28 17:57:15 +0800

    11.28

---
+++ js/variable.js
@@ -0,0 +1,105 @@
+console.log("HELLO WORLD");
+
+let ner = "Zulaa";
+let age = 26;
+
+console.log(ner);
+console.log(age);
+
+console.log(typeof(ner));
+console.log(typeof(age));
+//1
+
+// Comments can make code readable
+
+//2
+
+// Javascript lesson begin
+
+//3
+
+/* Comments 
+can 
+make 
+code 
+readable */
+
+//4
+
+let num = 123;
+let str = "string";
+let bool = false;
+let undef;
+let nll = null;
+
+//5
+
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(bool));
+console.log(typeof(undef));
+console.log(typeof(nll));
+
+//6
+
+let utgagui;
+
+//7
+
+let utga = 1;
+
+//8
+
+let firstname = "munkhzul"; 
+let lastname = "bayarkhuu";
+let country = "mongol"
+let nas = 26;
+
+//9 
+
+let uuriinner = "munkhzul", ovog = "bayarkhuu", uls = "mongol";
+
+//10
+
+let myage = 26, yourage = 20;
+
+//11
+
+let x = 4, y = 3, z = 5;
+console.log("x =" + x);
+console.log("y =" + y);
+console.log("z =" + z);
+let prim = (x + y + z);
+console.log("gurwaljnii primeter =" + prim);
+
+//12
+
+let r = 5;
+const phi = 3.14;
+let talbai = ( phi * r ** 2);
+console.log("r =" + r);
+console.log("p =" + phi);
+console.log("toirgiin talbai =" + talbai);
+
+//13
+
+
+//14
+let ugugdul = 15;
+let bit = ((1024 ** 3) * ugugdul * 8);
+console.log("15Gbyte =" + bit + "bit");
+
+//22
+
+let too = prompt("3 orontoi too oruulna uu");
+let uldegdel = (too % 100);
+let hariu = (too - uldegdel);
+console.log(hariu/100);
+
+
+
+
+
+
+
+
